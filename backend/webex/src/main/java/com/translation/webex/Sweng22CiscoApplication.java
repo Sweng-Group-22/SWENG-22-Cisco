a96c0086813befd9e4ea7f25f3d40494aee0b833
GMoylan02 <gerardmoylan02@gmail.com>
2023-02-13 22:12:37 +0000

    Initialised a java spring project
    Just initialised a java spring project using the spring initialiser. No code written yet

---
+++ backend/webex/src/main/java/com/translation/webex/Sweng22CiscoApplication.java
@@ -0,0 +1,13 @@
+package com.translation.webex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sweng22CiscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sweng22CiscoApplication.class, args);
+	}
+
+}
---
+++ backend/webex/src/test/java/com/translation/webex/Sweng22CiscoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translation.webex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sweng22CiscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

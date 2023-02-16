ebc5fa79b2d2cabeadf64fae402e1ea9fb6559df
HaoyuWww <79248601+HaoyuWww@users.noreply.github.com>
2023-02-16 13:15:19 +0000

    Update csv.java

--- backend/webex/src/main/csv.java
+++ backend/webex/src/main/csv.java
@@ -9,8 +9,8 @@ public class App {
 
 	public static void main(String[] args) {
 		Workbook wb = new Workbook();
-		String path = "D:\\tmp\\java\\excel\\aaa.xlsm";
-		String dstpath = "D:\\tmp\\java\\excel\\aaa.csv";
+		String path = "./excel/aaa.xlsm";
+		String dstpath = "./excel/aaa.csv";
 		wb.loadFromFile(path);
 		Worksheet sheet = wb.getWorksheets().get(0);
 		sheet.saveToFile(dstpath,",", Charset.forName("UTF-8"));

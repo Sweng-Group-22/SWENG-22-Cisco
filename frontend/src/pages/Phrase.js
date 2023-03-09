59202ce78fd4f5283dc619fdd7788581ecd51295
Mia Cimas <cimasm@tcd.ie>
2023-03-09 14:10:08 +0000

    Translation box component.

--- frontend/src/pages/Phrase.js
+++ frontend/src/pages/Phrase.js
@@ -4,31 +4,14 @@ import Footer from '../components/Footer'
 import AddButton from '../components/AddButton'
 import '../styles/Phrase.css'
 import LikeDislike from '../components/LikeDislike'
+import TranslationBox from '../components/TranslationBox'
 
 export default function Phrase() {
   return (
     <>
         <Header/>
-        <div className='phrase-box'>
-            <div className='phrase-title'>Translation 1</div>
-            <div className='phrase-content'>
-                <div className='phrase-line'></div>
-                <div className='phrase-translation'>amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris</div>
-            </div>
-            <div className='phrase-like-dislike'>
-                <LikeDislike/>
-            </div>
-        </div>
-        <div className='phrase-box'>
-            <div className='phrase-title'>Translation 2</div>
-            <div className='phrase-content'>
-                <div className='phrase-line'></div>
-                <div className='phrase-translation'>purus non enim praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam</div>
-            </div>
-            <div className='phrase-like-dislike'>
-                <LikeDislike/>
-            </div>
-        </div>
+        <TranslationBox/>
+        <TranslationBox/>
         <AddButton/>
         <Footer/>
         

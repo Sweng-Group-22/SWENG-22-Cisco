b1a908d2a7a15d0645b69f11862628f8baf7f4d6
FinnStrike <finnstrike7@gmail.com>
2023-03-14 13:10:56 +0000

    Fixed container issue with translation box

--- frontend/src/components/TranslationBox.js
+++ frontend/src/components/TranslationBox.js
@@ -5,18 +5,18 @@ import LikeDislike from '../components/LikeDislike'
 export default function Phrase() {
     return (
       <>
-        <div className='phrase-box'>
+        <div className='translation-box'>
           <div class='header'>
             <h1>Translated phrase</h1>   {/* Needs to be changed after discussion with backend */}
           </div>
-          <div className='phrase-content'>
-            <div className='phrase-vertical-line'></div>
-            <div className='phrase-translation'>
+          <div className='translation-content'>
+            <div className='translation-vertical-line'></div>
+            <div className='translation-translation'>
               Amet nisl suscipit adipiscing bibendum est ultricies 
               integer quis auctor elit sed vulputate mi sit amet mauris.
             </div>                           {/* Needs to be changed after discussion with backend */}
           </div>
-          <div className='phrase-like-dislike'><LikeDislike/></div>
+          <div className='translation-like-dislike'><LikeDislike/></div>
         </div>
       </>
     )

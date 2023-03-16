e7c158fa934cea49e2f150888f6ec418d4586274
GMoylan02 <gerardmoylan02@gmail.com>
2023-03-16 14:42:58 +0000

    Wrote method to query the db by letter
    Wrote a method to query the database and return an array of english phrases beginning with the case-insensitive letter parameter passed into the method. The idea being that it will connect to the menu on the frontend with all the letters.
    Not really sure how to query a mongoDB database so I'd appreciate it if someone with a bit more knowledge had a look over it.

--- backend/webex/src/main/java/com/translation/webex/comp/TranslationReadWrapper.java
+++ backend/webex/src/main/java/com/translation/webex/comp/TranslationReadWrapper.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
+import java.util.ArrayList;
 
 import com.translation.webex.entity.Translation;
 
@@ -24,5 +25,12 @@ public class TranslationReadWrapper {
 
         return result;
     }
-	
+    //Honestly doubtful this works at all since I don't really know how databases work
+    //Is meant to return an array of english phrases starting with the case-insensitive "letter" parameter
+    public Translation[] queryByLetter(char letter)
+    {
+        Query query = new Query(Criteria.where("englishPhrase").regex("/\b" + letter + "[A-z]*/g"));
+        Translation[] test = mongoTemplate.find(query, Translation.class, COLLECTION_NAME).toArray(new Translation[0]);
+        return test;
+    }
 }

de43c4cecd921c2f2f773a34fa38866e2673260e
HaoyuWww <79248601+HaoyuWww@users.noreply.github.com>
2023-04-10 17:39:56 +0100

    Update VoteDao.java

--- backend/webex/src/main/java/com/translation/webex/dao/VoteDao.java
+++ backend/webex/src/main/java/com/translation/webex/dao/VoteDao.java
@@ -1,10 +1,16 @@
 package com.translation.webex.dao;
 
+import java.util.List;
+
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.translation.webex.entity.Vote;
 
-public interface VoteDao extends MongoRepository<Vote, Integer> {
+public interface VoteDao extends MongoRepository<Vote, ObjectId> {
 
+	Vote findBySegmentAndLanguage(String segment,String language);
 	
+	
+	List<Vote> findBySegmentIn(List<String> seg);
 }

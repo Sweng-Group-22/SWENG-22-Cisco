4a8c951cf326525e710114732b2f66d458385886
FinnStrike <murphf15@tcd.ie>
2023-04-14 12:22:40 +0100

    Reverted changes from previous commit

--- frontend/src/components/GetPhrasesBox.js
+++ frontend/src/components/GetPhrasesBox.js
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom'
 import Modal from './Modal';
 
 export default function GetPhrasesBox(props) {
-  const {data} = useQuery(['browse',props.EnglishPhrase],()=>searchTranslation(props.EnglishPhrase))
-  console.log(data)
   
   return (
     <>
@@ -18,7 +16,7 @@ export default function GetPhrasesBox(props) {
           <div className='get-phrases-box-view-suggest'>
 
             <div className='get-phrases-box-view'>
-              <Link style={{textDecoration: 'none'}} class='view' to={`/phrase/${props.EnglishPhrase}`} onClick={()=>props.setPhrase(data[0])}>View existing translations</Link>
+              <Link style={{textDecoration: 'none'}} class='view' to='/phrase/phrase1'>View existing translations</Link>
             </div>
 
             <div className='get-phrases-box-suggest' onClick={props.showModal}>
--- frontend/src/pages/GetPhrases.js
+++ frontend/src/pages/GetPhrases.js
@@ -7,7 +7,7 @@ import { useState } from 'react'
 import { useQuery } from 'react-query'
 import { getTranslations } from '../api/Translation'
 
-export default function GetPhrases(props) {
+export default function GetPhrases() {
 
   const [openModal, setOpenModal] = useState(false);
   function showModal() {
@@ -20,8 +20,6 @@ export default function GetPhrases(props) {
   // function usePhrases(letter){ return useQuery(['phrases', letter], () => getTranslations(letter))}
   // const {data} = usePhrases('')
   // console.log(data.data)
-  const {data} = useQuery(['browse',props.Letter],()=>searchTranslation(props.Letter))
-  console.log(data)
 
   return (
     <>
@@ -31,13 +29,13 @@ export default function GetPhrases(props) {
         </div>
         <div class="row3">
           <div className='column3'>
-            <GetPhrasesBox idn={1} openModal={openModal} showModal={showModal} hideModal={hideModal} EnglishPhrase={"Video"}/>
+            <GetPhrasesBox idn={1} openModal={openModal} showModal={showModal} hideModal={hideModal} EnglishPhrase={"People"}/>
             <GetPhrasesBox idn={2} openModal={openModal} showModal={showModal} hideModal={hideModal} EnglishPhrase={"Personal Insights"}/>
           </div>
         
           <div className='column3'>
-            <GetPhrasesBox idn={3} openModal={openModal} showModal={showModal} hideModal={hideModal} EnglishPhrase={"Camera"}/>
-            <GetPhrasesBox idn={4} openModal={openModal} showModal={showModal} hideModal={hideModal} EnglishPhrase={"People"}/>
+            <GetPhrasesBox idn={3} openModal={openModal} showModal={showModal} hideModal={hideModal} EnglishPhrase={"Personal Room Invite"}/>
+            <GetPhrasesBox idn={4} openModal={openModal} showModal={showModal} hideModal={hideModal} EnglishPhrase={"Pinned Messages"}/>
           </div>
         </div>
         
